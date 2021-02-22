b59a3412e605e76dc218120c00051ab7af70a90f
Hong Tran <jessietran@fb.com>
2021-02-22 11:12:09 -0800

    added shadow for buttons

--- scenario/scenario1/conversation2.js
+++ scenario/scenario1/conversation2.js
@@ -284,10 +284,10 @@ function addButtons(){
         if (buttons[i].text){
             let button = document.createElement('div')
             button.style.padding = '10px 20px'
-            button.style.backgroundColor = '#E85552'
+            button.style.backgroundColor = '#111F47'
             button.style.color = 'white'
             button.style.borderRadius = '20px'
-            button.style.boxShadow = 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;'
+            button.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px'
             button.appendChild(document.createTextNode(buttons[i].text))
             button.style.cursor = 'pointer'
             button.style.marginRight = '20px'
