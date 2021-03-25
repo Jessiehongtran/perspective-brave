1ae2fce091d86c984bf7398a9a77af6151806660
Hong Tran <jessietran@fb.com>
2021-03-25 14:44:44 -0700

    added VO for bias summary

--- games/balloon/end.js
+++ games/balloon/end.js
@@ -1,3 +1,20 @@
+let audio 
+
+function speak(file){
+    audio = new Audio(file);
+    audio.volume = 1;
+    audio.play()
+    audio.onloadedmetadata = function() {
+        return  audio.duration
+    };
+}
+
+function stopVO(){
+    if (audio){
+        audio.pause()
+    }
+}
+
 const texts = [
     "When bias is perceived in the workplace, 3 in ten people say they plan to leave their current job.",
     "And the same number -roughly 3 in 10- report withholding ideas and market solutions."
@@ -19,15 +36,24 @@ document.getElementsByClassName('dots')[0].style.width = '10%'
 let curId = 0
 
 function showText(id){
+    if (audio){
+        stopVO()
+    }
     info.innerHTML = texts[id]
     dots[id].setAttribute('class', "each-dot active")
     dots[id].setAttribute('aria-label', "Active")
-    dots[curId].setAttribute('class', "each-dot")
-    dots[curId].setAttribute('aria-label', "Inactive")
+    if (id !== curId){
+        dots[curId].setAttribute('class', "each-dot")
+        dots[curId].setAttribute('aria-label', "Inactive")
+    }
     curId = id
+    speak(`../../asset/VOfiles/PerspectivesVO_bias_summary${id + 1}.wav`)
 }
 
 function nextSlide(){
+    if (audio){
+        stopVO()
+    }
     if (curId + 1 < texts.length){
         const nextId = curId + 1
         showText(nextId)
@@ -38,6 +64,9 @@ function nextSlide(){
 }
 
 function preSlide(){
+    if (audio){
+        stopVO()
+    }
     if (curId - 1 >= 0){
         const nextId = curId - 1
         showText(nextId)
